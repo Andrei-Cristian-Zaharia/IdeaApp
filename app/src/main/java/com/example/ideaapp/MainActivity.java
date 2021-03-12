71fcacee3a81f9cad7f70c58dfe90a12bce94654
Iancu Teodor <iancuteodor14@yahoo.com>
2021-03-12 19:10:10 +0200

    test_layout

--- app/src/main/java/com/example/ideaapp/MainActivity.java
+++ app/src/main/java/com/example/ideaapp/MainActivity.java
@@ -1,16 +1,65 @@
 package com.example.ideaapp;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    EditText username;
+    Button button;
+    String usernamee ;
+
+   /* public static final String SHARED_PREFS = " sharedPrefs";
+    public static final String TEXT = "text ";
+
+    */
+
+   // private String text;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_layout);
 
-        Database db = new Database(this);
+      /*  username = (EditText) findViewById(R.id.username) ;
+        button = (Button)   findViewById(R.id.button);
+
+
+
+
+     button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SaveData();
+            }
+        });
+
+       */
+
+
+
      }
+
+    /* void SaveData ()  {
+        usernamee =  username.getText().toString();
+         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS , MODE_PRIVATE);
+         SharedPreferences.Editor editor = sharedPreferences.edit();
+
+         editor.putString(TEXT,username.getText().toString());
+
+         text = sharedPreferences.getString(TEXT , " ");
+         editor.apply();
+
+
+         username.setText(text);
+
+        Toast.makeText(this, "Data saved",Toast.LENGTH_SHORT).show();
+
+     }
+
+     */
 }
