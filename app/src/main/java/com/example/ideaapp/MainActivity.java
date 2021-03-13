6090fc80b78da881bdd2b5997c4b194ef75aab2f
Asar <zaharia.andrei2010@gmail.com>
2021-03-13 12:16:30 +0200

    First-commit

--- app/src/main/java/com/example/ideaapp/MainActivity.java
+++ app/src/main/java/com/example/ideaapp/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.ideaapp;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,21 +18,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 
+import java.util.List;
+
 import Features.Database;
 import Features.Idea_Adapter;
+import Models.UserModel;
 
 public class MainActivity extends AppCompatActivity  {
 
     EditText username;
     Button button;
     String usernamee ;
-    Switch test;
+    TextView errorT ;
+
     public static final String SHARED_PREFS = " sharedPrefs";
     public static final String TEXT = "text ";
-
+    boolean canSave = true;
 
     private String text;
-
 
     ListView listView;
 
@@ -39,31 +44,55 @@ public class MainActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Database db = new Database(this);
+
         LoadData();
         if(text.isEmpty()){
             setContentView(R.layout.activity_main);
+
             username = (EditText) findViewById(R.id.username) ;
             button = (Button)   findViewById(R.id.button);
+            errorT = (TextView) findViewById(R.id.error);
 
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    SaveData();
 
-                    openActivity();
+                    if (username.getText().toString().isEmpty())
+                        errorT.setText("Username can't be empty");
+                    else {
+                        List<UserModel> users = Database.getAllUsers();
 
+                        for (UserModel user : users)
+                            if (user.getUsername().equals(username.getText().toString()))
+                                canSave = false;
+
+                        if (canSave) {
+                             Database.InsertUser(username.getText().toString());
+                             SaveData();
+
+                             openActivity();
+                        } else errorT.setText("User already exist !");
+                    }
                 }
             });
         }
         else
         {
+            SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS , MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+
+            editor.putString(TEXT, null);
+
+            text = sharedPreferences.getString(TEXT , " ");
+            editor.apply();
+
             openActivity();
         }
-
      }
 
   void SaveData ()  {
-        usernamee =  username.getText().toString();
+         usernamee =  username.getText().toString();
          SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS , MODE_PRIVATE);
          SharedPreferences.Editor editor = sharedPreferences.edit();
 
