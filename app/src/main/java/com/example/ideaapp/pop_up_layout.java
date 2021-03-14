3ff0a70d932a6681776b9a57e6006c5d4b39bcc6
Iancu Teodor <iancuteodor14@yahoo.com>
2021-03-14 15:39:12 +0200

    ola

--- app/src/main/java/com/example/ideaapp/pop_up_layout.java
+++ app/src/main/java/com/example/ideaapp/pop_up_layout.java
@@ -17,14 +17,12 @@ public class pop_up_layout extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pop_up_layout);
         Intent intent = getIntent();
-        String text1 = intent.getStringExtra(Main_display_activity.EXTRA_TEXT1);
-        String text2 = intent.getStringExtra(Main_display_activity.EXTRA_TEXT2);
+
 
         TextView textview1 = (TextView) findViewById(R.id.name1);
         TextView textview2 = (TextView) findViewById(R.id.description1);
 
-        textview1.setText(text1);
-        textview2.setText(text2);
+
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
