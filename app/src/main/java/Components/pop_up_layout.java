5a3283edc86563d5ad9bf9f48f2db91a067c632c
Asar <zaharia.andrei2010@gmail.com>
2021-03-26 11:26:40 +0200

    Refresh + like
    hehehe

--- app/src/main/java/Components/pop_up_layout.java
+++ app/src/main/java/Components/pop_up_layout.java
@@ -6,17 +6,24 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.ideaapp.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import Components.Main_display_activity;
+import Features.Database;
+import Models.Idea;
 
 
 public class pop_up_layout extends AppCompatActivity {
+
+    FloatingActionButton addButton;
+    private boolean isLiked;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +33,26 @@ public class pop_up_layout extends AppCompatActivity {
 
         TextView textview1 = (TextView) findViewById(R.id.name1);
         TextView textview2 = (TextView) findViewById(R.id.description1);
+        addButton = findViewById(R.id.addButton);
 
         String text1 = intent.getStringExtra(Main_display_activity.EXTRA_TEXT1);
         String text2 = intent.getStringExtra(Main_display_activity.EXTRA_TEXT2);
 
         textview1.setText(text1);
         textview2.setText(text2);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isLiked = Database.isIdeaLiked(text1);
+                if (!isLiked) {
+                   Database.giveLike(text1);
+                }
+                else {
+                    Database.removeLike(text1);
+                }
+            }
+        });
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
--- app/src/main/java/Models/UserModel.java
+++ app/src/main/java/Models/UserModel.java
@@ -1,7 +1,9 @@
 package Models;
 
+import io.realm.RealmList;
 import io.realm.RealmObject;
 import io.realm.annotations.PrimaryKey;
+import io.realm.annotations.Required;
 
 import org.bson.types.ObjectId;
 
@@ -9,13 +11,17 @@ public class UserModel extends RealmObject {
 
     @PrimaryKey
     private ObjectId _id;
+    @Required
+    private RealmList<String> liked_ideas;
     private String partition_id;
     private String username;
 
-    public UserModel() { _id = new ObjectId(); partition_id = "IdeaApp"; }
+    public UserModel() { _id = new ObjectId(); partition_id = "IdeaApp"; liked_ideas = new RealmList<String>(); }
 
     // Standard getters & setters
     public ObjectId get_id() { return _id; }
     public String getUsername() { return username; }
+    public RealmList<String> getLiked_ideas() { return liked_ideas; }
+    public void setLiked_ideas(RealmList<String> liked_ideas) { this.liked_ideas = liked_ideas; }
     public void setUsername(String username) { this.username = username; }
 }
