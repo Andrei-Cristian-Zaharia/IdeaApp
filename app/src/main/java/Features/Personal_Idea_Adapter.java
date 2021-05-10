bb0df4db8b174ab7eab936b088cf7c9d35cb4c92
Asar <zaharia.andrei2010@gmail.com>
2021-05-10 20:00:07 +0300

    v2

--- app/src/main/java/Features/Personal_Idea_Adapter.java
+++ app/src/main/java/Features/Personal_Idea_Adapter.java
@@ -4,13 +4,16 @@ import android.annotation.SuppressLint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.ideaapp.R;
 
+import Fragments.FragmentAccount;
 import Models.Idea;
 import io.realm.OrderedRealmCollection;
 import io.realm.RealmRecyclerViewAdapter;
@@ -44,6 +47,7 @@ public class Personal_Idea_Adapter extends RealmRecyclerViewAdapter<Idea, Recycl
     private static class ProfileIdeaClass extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         private final TextView nameText, descriptionText;
+        private Button editButton, deleteButton;
 
         private final OnNoteListener onNoteListener;
 
@@ -52,6 +56,8 @@ public class Personal_Idea_Adapter extends RealmRecyclerViewAdapter<Idea, Recycl
 
             nameText = view.findViewById(R.id.name_idea_text);
             descriptionText = view.findViewById(R.id.description_idea_text);
+            editButton = view.findViewById(R.id.edit_button);
+            deleteButton = view.findViewById(R.id.delete_button);
 
             onNoteListener = _onNoteListener;
 
@@ -62,6 +68,21 @@ public class Personal_Idea_Adapter extends RealmRecyclerViewAdapter<Idea, Recycl
         public void bind(Idea idea){
             nameText.setText(idea.get_nume());
             descriptionText.setText(idea.get_description());
+
+            editButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(v.getContext(), "You are editing this idea.", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            deleteButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(v.getContext(), "Idea deleted !", Toast.LENGTH_SHORT).show();
+                }
+            });
+
         }
 
         @Override
